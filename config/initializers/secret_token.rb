baf9f040593df29327525016cdf68c044720b8c5
Sunny Github <sunnygithub@hotmail.com>
2013-06-19 17:13:00 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fdd19153f58ebc9304a6efa8c9feb00c32339234fed29d1c2e711c04fad0ab9f86cc989581f9f3ca34725fed7e42ce0f5d090e7a3ed1422662f434f0928201a7'
